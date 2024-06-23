3c0131b7344082b4346582c44e65cff616580b6e
elamathi-elangovan <elaworkuiux@gmail.com>
2024-06-23 15:53:48 +0530

    commit changes

--- app/src/main/java/com/example/ela_1/MainActivity.kt
+++ app/src/main/java/com/example/ela_1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ela_1
 
 import android.os.Bundle
+import android.util.Log.i
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +19,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun myClickHandler(view: View) {
+        i("MainActivity","button clicked")
+    }
+
 }
